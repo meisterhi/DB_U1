e59fd5a0b326e026482108a92d727dd7a95c9d5c
meisterhi <foenixcraft@gmail.com>
2023-11-01 22:59:49 +0100

    it works now, but I need the console

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,39 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Main {
-
-    
     public static void main(String[] args) {
         
         final Properties props = new Properties();
         props.setProperty("user", "student2");
         props.setProperty("password", "student");
-        props.setProperty("ssl", "true");
+        props.setProperty("ssl", "false");
         final String url = "jdbc:postgresql://141.64.89.149/world";
-        
-
+    
     try{
         Connection conn = DriverManager.getConnection(url, props);    
+        getQuarry("SELECT COUNT(*) FROM CITY", conn);
+        System.out.println("-------");
+        getQuarry("SELECT name,population FROM CITY Order by population ASC ", conn);
 
+    } catch (SQLException  e) {
+        System.err.println("Failed establishing Connection \n" + e);      
+    }
+    }
+    public static void getQuarry(String m, Connection conn) throws SQLException{
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM mytable WHERE columnfoo = 500"); //TODO: add Quarry
+        ResultSet rs = st.executeQuery(m);
         while (rs.next()) {
-        System.out.print("Column 1 returned "); // TODO: Add meaning
-        System.out.println(rs.getString(1));
+            //System.out.print("Row " +rs.getRow() + " returned"); // TODO: Add meaning
+            int colIndex = rs.getMetaData().getColumnCount();
+            while (colIndex > 0) {
+                System.out.print(" "+rs.getString(colIndex) );
+                colIndex--;
             }
+            System.err.println();
+            
+        }
         rs.close();
         st.close();
-    } catch (SQLException  e) {
-        // TODO: handle exception
-        System.err.println("Failed establishing Connection \n" + e);
-        
-    }
-
     }
 
 }
